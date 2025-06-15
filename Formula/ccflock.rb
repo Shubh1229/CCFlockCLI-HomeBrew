3da95e9b9dbb512937f25681faa7d44027e9a235
Arihant Singh <Shubh1229@users.noreply.github.com>
2025-06-15 14:37:28 -0400

    updated for linux

--- Formula/ccflock.rb
+++ Formula/ccflock.rb
@@ -1,8 +1,15 @@
 class Ccflock < Formula
   desc "CLI for interacting with the Cloud Connected Flock project"
   homepage "https://github.com/Shubh1229/ccflock-cli"
-  url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1/ccflock"
-  sha256 "804227f54635e855f2d96ca5dc7092e2096b64f3d5fd36e3382f80f2f8f1ba64"
+
+  if OS.mac?
+    url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1/ccflock"
+    sha256 "804227f54635e855f2d96ca5dc7092e2096b64f3d5fd36e3382f80f2f8f1ba64"
+  elsif OS.linux?
+    url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1-linux/ccflock"
+    sha256 "0f0c2b8dbb870acd62244431fe7353a397377f976fc5e361111a71f5c3504752"
+  end
+
   version "0.0.1"
 
   def install
