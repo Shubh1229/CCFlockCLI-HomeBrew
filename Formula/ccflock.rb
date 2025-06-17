35d0f79bc40cfb9ef9a305f69e23f1aac45deaa1
Arihant Singh <Shubh1229@users.noreply.github.com>
2025-06-17 02:02:05 -0400

    fixed json serialization problem

--- Formula/ccflock.rb
+++ Formula/ccflock.rb
@@ -4,7 +4,7 @@ class Ccflock < Formula
 
   if OS.mac?
     url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.1.1/ccflock"
-    sha256 "e88a1607e23d6547fdb4e4cd71a6f7b31f531f5a92d358c2ab4fe7c774959a87"
+    sha256 "2d30dd6a8a8f81ea495872d4caef8fc1dfb1d1c00f7a4ffb0958542ee5e28afa"
   elsif OS.linux?
     url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1-linux/ccflock"
     sha256 "0f0c2b8dbb870acd62244431fe7353a397377f976fc5e361111a71f5c3504752"
