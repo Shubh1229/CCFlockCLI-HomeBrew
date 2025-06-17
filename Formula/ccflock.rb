e67db4a2b78c1911dad5c201857054c06fad7571
Arihant Singh <Shubh1229@users.noreply.github.com>
2025-06-17 01:17:58 -0400

    new binary added snake

--- Formula/ccflock.rb
+++ Formula/ccflock.rb
@@ -3,8 +3,8 @@ class Ccflock < Formula
   homepage "https://github.com/Shubh1229/ccflock-cli"
 
   if OS.mac?
-    url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.2/ccflock"
-    sha256 "0daf2a6ee930fff5838484c4ecf59e91c6057f5f036b5d643999e74042e2fb63"
+    url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.1.1/ccflock"
+    sha256 "0b5818b431f5828b991a64fafb93029517fdce9ba7f6102da9c726d077520f91"
   elsif OS.linux?
     url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1-linux/ccflock"
     sha256 "0f0c2b8dbb870acd62244431fe7353a397377f976fc5e361111a71f5c3504752"
