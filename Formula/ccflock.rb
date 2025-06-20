ec70b9bd5454d76a8d1c4faa466141c01912cab4
Arihant Singh <Shubh1229@users.noreply.github.com>
2025-06-19 23:40:49 -0400

    Update ccflock.rb

--- Formula/ccflock.rb
+++ Formula/ccflock.rb
@@ -4,7 +4,7 @@ class Ccflock < Formula
 
   if OS.mac?
     url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v1.1.3/ccflock"
-    sha256 "c48b148a987963cc4fde2fa36f17664e4fedb1f5588916c7f840d371ab4a6f63"
+    sha256 "9767fe6b4240aa2c4dd7d1053d020c531978050bbe7ec28a63a43cc034f4ccde"
   elsif OS.linux?
     url "https://github.com/Shubh1229/CCFlockCLI/releases/download/v0.0.1-linux/ccflock"
     sha256 "0f0c2b8dbb870acd62244431fe7353a397377f976fc5e361111a71f5c3504752"
